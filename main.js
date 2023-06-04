cddcfd400b019c72ca8cc8b19fbb142b5cd93b69
Esleiter <57963142+Esleiter@users.noreply.github.com>
2023-06-04 17:36:20 +0000

    delete - pwa

--- main.js
+++ main.js
@@ -84,12 +84,3 @@ const getWeatherInPositionGps = async (position) => {
 window.getWeatherInPositionGps = getWeatherInPositionGps;
 
 getWeatherInPositionIp();
-
-if ("serviceWorker" in navigator) {
-    window.addEventListener("load", function() {
-      navigator.serviceWorker
-        .register("/serviceWorker.js")
-        .then(res => console.log("service worker registered"))
-        .catch(err => console.log("service worker not registered", err))
-    })
-  }
